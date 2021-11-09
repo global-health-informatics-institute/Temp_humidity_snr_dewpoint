bc53233fa70a7d645625c53708b93d9e698c520e
Duntar <mwabilungu@gmail.com>
2021-11-09 08:44:25 +0200

    Add files via upload

--- Tem_Humid_sensor_dewPoint/Tem_Humid_sensor_dewPoint.ino
+++ Tem_Humid_sensor_dewPoint/Tem_Humid_sensor_dewPoint.ino
@@ -1,12 +1,6 @@
 
 #include <Wire.h>
 
-#define WIFI_SSID "Fadenlauf-2"
-#define WIFI_PASSWORD "watchout"
-
-#define MQTT_HOST IPAddress(192, 168, 0, 59)
-#define MQTT_PORT 1883
-
 #define si7021 0x40 //set humidity sensor address
 
 double Humidity = 0.00;
@@ -17,6 +11,24 @@ int X0,X1,temp;
 double X,X_out;
 
 const int MeasureHumid = 0xE5; //Command to sent to the si7021 to read the humidity 
+
+const int segment1_1 = 5;
+const int segment1_2= 18; 
+const int segment1_3 =19;
+const int segment1_4 =2;
+const int segment1_5 =4;
+const int segment1_6 =17;
+const int segment1_7 =16;
+const int segment2_1 =32;
+const int segment2_2 =33;
+const int segment2_3 =25;
+const int segment2_4 =26;
+const int segment2_5 =27    ;
+const int segment2_6 =12;
+const int segment2_7 =14;
+const int ground  = 15;
+int seg1;
+int seg2;
 
 bool WireEnd() {
   unsigned char err;
@@ -67,6 +79,23 @@ double GetTemp(int SDA_Pin, int SLC_pin) {
 
 void setup() {
   Serial.begin(115200);
+
+  pinMode(segment1_1, OUTPUT);
+  pinMode(segment1_2, OUTPUT);
+  pinMode(segment1_3, OUTPUT);
+  pinMode(segment1_4, OUTPUT);
+  pinMode(segment1_5, OUTPUT);
+  pinMode(segment1_6, OUTPUT);
+  pinMode(segment1_7, OUTPUT);
+  pinMode(segment2_1, OUTPUT);
+  pinMode(segment2_2, OUTPUT);
+  pinMode(segment2_3, OUTPUT);
+  pinMode(segment2_4, OUTPUT);
+  pinMode(segment2_5, OUTPUT);
+  pinMode(segment2_6, OUTPUT);
+  pinMode(segment2_7, OUTPUT);
+  pinMode(ground,    OUTPUT);
+  allPinsLOW();
 }
 
 void loop() {
@@ -78,4 +107,258 @@ void loop() {
   Serial.print("Temperature % C =" + String(Temperature));
   Serial.print(" Humidity % RH =" + String(Humidity));
   Serial.println(" Dew Point % DP =" + String(Dew_point));
+  updateLCD(Dew_point);
+  delay(20);
+  alternate(); 
+  
   }
+
+//Function to display Dew point on LCD
+void updateLCD(int dewpoint)
+{
+  
+  seg1 = (dewpoint/10)%10; // Extracting the first digit from dew point
+  seg2 = dewpoint%10; // Extracting the second digit from dew point
+
+  //Print First digit
+  if(seg1 == 0){
+   digitalWrite(segment1_1, HIGH); 
+   digitalWrite(segment1_2, HIGH);
+   digitalWrite(segment1_3, HIGH);
+   digitalWrite(segment1_4, HIGH);
+   digitalWrite(segment1_5, HIGH);
+   digitalWrite(segment1_6, HIGH);
+   digitalWrite(segment1_7, LOW);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg1 == 1){
+   digitalWrite(segment1_1, LOW); 
+   digitalWrite(segment1_2, HIGH);
+   digitalWrite(segment1_3, HIGH);
+   digitalWrite(segment1_4, LOW);
+   digitalWrite(segment1_5, LOW);
+   digitalWrite(segment1_6, LOW);
+   digitalWrite(segment1_7, LOW);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg1 == 2){
+   digitalWrite(segment1_1, HIGH); 
+   digitalWrite(segment1_2, HIGH);
+   digitalWrite(segment1_3, LOW);
+   digitalWrite(segment1_4, HIGH);
+   digitalWrite(segment1_5, HIGH);
+   digitalWrite(segment1_6, LOW);
+   digitalWrite(segment1_7, HIGH);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg1 == 3){
+   digitalWrite(segment1_1, HIGH); 
+   digitalWrite(segment1_2, HIGH);
+   digitalWrite(segment1_3, HIGH);
+   digitalWrite(segment1_4, HIGH);
+   digitalWrite(segment1_5, LOW);
+   digitalWrite(segment1_6, LOW);
+   digitalWrite(segment1_7, HIGH);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg1 == 4){
+   digitalWrite(segment1_1, LOW); 
+   digitalWrite(segment1_2, HIGH);
+   digitalWrite(segment1_3, HIGH);
+   digitalWrite(segment1_4, LOW);
+   digitalWrite(segment1_5, LOW);
+   digitalWrite(segment1_6, HIGH);
+   digitalWrite(segment1_7, HIGH);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg1 == 5){
+   digitalWrite(segment1_1, HIGH); 
+   digitalWrite(segment1_2, LOW);
+   digitalWrite(segment1_3, HIGH);
+   digitalWrite(segment1_4, HIGH);
+   digitalWrite(segment1_5, LOW);
+   digitalWrite(segment1_6, HIGH);
+   digitalWrite(segment1_7, HIGH);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg1 == 6){
+   digitalWrite(segment1_1, HIGH); 
+   digitalWrite(segment1_2, LOW);
+   digitalWrite(segment1_3, HIGH);
+   digitalWrite(segment1_4, HIGH);
+   digitalWrite(segment1_5, HIGH);
+   digitalWrite(segment1_6, HIGH);
+   digitalWrite(segment1_7, HIGH);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg1 == 7){
+   digitalWrite(segment1_1, HIGH); 
+   digitalWrite(segment1_2, HIGH);
+   digitalWrite(segment1_3, HIGH);
+   digitalWrite(segment1_4, LOW);
+   digitalWrite(segment1_5, LOW);
+   digitalWrite(segment1_6, LOW);
+   digitalWrite(segment1_7, LOW);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg1 == 8){
+   digitalWrite(segment1_1, HIGH); 
+   digitalWrite(segment1_2, HIGH);
+   digitalWrite(segment1_3, HIGH);
+   digitalWrite(segment1_4, HIGH);
+   digitalWrite(segment1_5, HIGH);
+   digitalWrite(segment1_6, HIGH);
+   digitalWrite(segment1_7, HIGH);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg1 == 9){
+   digitalWrite(segment1_1, HIGH); 
+   digitalWrite(segment1_2, HIGH);
+   digitalWrite(segment1_3, HIGH);
+   digitalWrite(segment1_4, HIGH);
+   digitalWrite(segment1_5, LOW);
+   digitalWrite(segment1_6, HIGH);
+   digitalWrite(segment1_7, HIGH);
+   digitalWrite(ground, LOW);
+  }
+
+  //Print Second digit
+  if(seg2 == 0){
+   digitalWrite(segment2_1, HIGH); 
+   digitalWrite(segment2_2, HIGH);
+   digitalWrite(segment2_3, HIGH);
+   digitalWrite(segment2_4, HIGH);
+   digitalWrite(segment2_5, HIGH);
+   digitalWrite(segment2_6, HIGH);
+   digitalWrite(segment2_7, LOW);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg2 == 1){
+   digitalWrite(segment2_1, LOW); 
+   digitalWrite(segment2_2, HIGH);
+   digitalWrite(segment2_3, HIGH);
+   digitalWrite(segment2_4, LOW);
+   digitalWrite(segment2_5, LOW);
+   digitalWrite(segment2_6, LOW);
+   digitalWrite(segment2_7, LOW);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg2 == 2){
+   digitalWrite(segment2_1, HIGH); 
+   digitalWrite(segment2_2, HIGH);
+   digitalWrite(segment2_3, LOW);
+   digitalWrite(segment2_4, HIGH);
+   digitalWrite(segment2_5, HIGH);
+   digitalWrite(segment2_6, LOW);
+   digitalWrite(segment2_7, HIGH);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg2 == 3){
+   digitalWrite(segment2_1, HIGH); 
+   digitalWrite(segment2_2, HIGH);
+   digitalWrite(segment2_3, HIGH);
+   digitalWrite(segment2_4, HIGH);
+   digitalWrite(segment2_5, LOW);
+   digitalWrite(segment2_6, LOW);
+   digitalWrite(segment2_7, HIGH);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg2 == 4){
+   digitalWrite(segment2_1, LOW); 
+   digitalWrite(segment2_2, HIGH);
+   digitalWrite(segment2_3, HIGH);
+   digitalWrite(segment2_4, LOW);
+   digitalWrite(segment2_5, LOW);
+   digitalWrite(segment2_6, HIGH);
+   digitalWrite(segment2_7, HIGH);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg2 == 5){
+   digitalWrite(segment2_1, HIGH); 
+   digitalWrite(segment2_2, LOW);
+   digitalWrite(segment2_3, HIGH);
+   digitalWrite(segment2_4, HIGH);
+   digitalWrite(segment2_5, LOW);
+   digitalWrite(segment2_6, HIGH);
+   digitalWrite(segment2_7, HIGH);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg2 == 6){
+   digitalWrite(segment2_1, HIGH); 
+   digitalWrite(segment2_2, LOW);
+   digitalWrite(segment2_3, HIGH);
+   digitalWrite(segment2_4, HIGH);
+   digitalWrite(segment2_5, HIGH);
+   digitalWrite(segment2_6, HIGH);
+   digitalWrite(segment2_7, HIGH);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg2 == 7){
+   digitalWrite(segment2_1, HIGH); 
+   digitalWrite(segment2_2, HIGH);
+   digitalWrite(segment2_3, HIGH);
+   digitalWrite(segment2_4, LOW);
+   digitalWrite(segment2_5, LOW);
+   digitalWrite(segment2_6, LOW);
+   digitalWrite(segment2_7, LOW);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg2 == 8){
+   digitalWrite(segment2_1, HIGH); 
+   digitalWrite(segment2_2, HIGH);
+   digitalWrite(segment2_3, HIGH);
+   digitalWrite(segment2_4, HIGH);
+   digitalWrite(segment2_5, HIGH);
+   digitalWrite(segment2_6, HIGH);
+   digitalWrite(segment2_7, HIGH);
+   digitalWrite(ground, LOW);
+  }
+  else if(seg2 == 9){
+   digitalWrite(segment2_1, HIGH); 
+   digitalWrite(segment2_2, HIGH);
+   digitalWrite(segment2_3, HIGH);
+   digitalWrite(segment2_4, HIGH);
+   digitalWrite(segment2_5, LOW);
+   digitalWrite(segment2_6, HIGH);
+   digitalWrite(segment2_7, HIGH);
+   digitalWrite(ground, LOW);
+  }  
+}
+
+void alternate()
+{
+   digitalWrite(segment1_1, LOW); 
+   digitalWrite(segment1_2, LOW);
+   digitalWrite(segment1_3, LOW);
+   digitalWrite(segment1_4, LOW);
+   digitalWrite(segment1_5, LOW);
+   digitalWrite(segment1_6, LOW);
+   digitalWrite(segment1_7, LOW);
+   digitalWrite(segment2_1, LOW); 
+   digitalWrite(segment2_2, LOW);
+   digitalWrite(segment2_3, LOW);
+   digitalWrite(segment2_4, LOW);
+   digitalWrite(segment2_5, LOW);
+   digitalWrite(segment2_6, LOW);
+   digitalWrite(segment2_7, LOW);
+   digitalWrite(ground, HIGH);
+}
+
+void allPinsLOW()
+{
+   digitalWrite(segment1_1, LOW); 
+   digitalWrite(segment1_2, LOW);
+   digitalWrite(segment1_3, LOW);
+   digitalWrite(segment1_4, LOW);
+   digitalWrite(segment1_5, LOW);
+   digitalWrite(segment1_6, LOW);
+   digitalWrite(segment1_7, LOW);
+   digitalWrite(segment2_1, LOW); 
+   digitalWrite(segment2_2, LOW);
+   digitalWrite(segment2_3, LOW);
+   digitalWrite(segment2_4, LOW);
+   digitalWrite(segment2_5, LOW);
+   digitalWrite(segment2_6, LOW);
+   digitalWrite(segment2_7, LOW);
+   digitalWrite(ground, LOW);
+}
